7ecce48cb10602be624a55fa37dc1a355c67926a
atharwa-24 <atharwakharkar@gmail.com>
2021-05-19 20:53:01 +0530

    .mp4 to .wav conversion

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import speech_recognition as sr
+import moviepy.editor as mp
+
 app = Flask(__name__)
 
 
@@ -15,13 +17,16 @@ def index():
             return redirect(request.url)
 
         if file:
+            clip = mp.VideoFileClip(r"video_recording.mp4")
+
+            clip.audio.write_audiofile(r"converted.wav")
             recognizer = sr.Recognizer()
-            audioFile = sr.AudioFile(file)
+            audioFile = sr.AudioFile("converted.wav")
             with audioFile as source:
                 data = recognizer.record(source)
             transcript = recognizer.recognize_google(data, key=None)
 
-    return render_template("index.html", transcript= transcript)
+    return render_template("index.html", transcript=transcript)
 
 
 if __name__ == "__main__":
