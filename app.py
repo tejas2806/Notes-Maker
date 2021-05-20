6700b164a7a8188ed60ea19ed3161d572f87ab46
atharwa-24 <atharwakharkar@gmail.com>
2021-05-20 23:14:44 +0530

    Integrated the summary transcript

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask, render_template, request, redirect
 import speech_recognition as sr
 import os
@@ -11,157 +9,154 @@ from nltk.cluster.util import cosine_distance
 import numpy as np
 import networkx as nx
 import nltk
+
 nltk.download('stopwords')
-import sys
 
 app = Flask(__name__)
 
 
-def silence_based_conversion(path):
-    song = AudioSegment.from_wav(path)
-
-    fh = open("recognized.txt", "w+")
-
-    # split track where silence is 0.7 seconds
-    # or more and get chunks
-    chunks = split_on_silence(song,
-                              # must be silent for at least 0.7 seconds
-                              # or 500 ms. adjust this value based on user
-                              # requirement. if the speaker stays silent for
-                              # longer, increase this value. else, decrease it.
-                              min_silence_len=700,
-
-                              # consider it silent if quieter than -42 dBFS
-                              # adjust this per requirement
-                              silence_thresh=-42
-                              )
-
-    try:
-        os.mkdir('audio_chunks')  # Here the data have to be directed to SQL DB
-    except(FileExistsError):
-        pass
-
-    os.chdir('audio_chunks')
-
-    i = 0
-    # process each chunk
-    for chunk in chunks:
-
-        # Create 0.5 seconds silence chunk
-        chunk_silent = AudioSegment.silent(duration=10)
-
-        # add 0.5 sec silence to beginning and
-        # end of audio chunk. This is done so that
-        # it doesn't seem abruptly sliced.
-        audio_chunk = chunk_silent + chunk + chunk_silent
-
-        print("saving chunk{0}.wav".format(i))
-
-        audio_chunk.export("./chunk{0}.wav".format(i), bitrate='192k', format="wav")
-
-        filename = 'chunk' + str(i) + '.wav'
-
-        print("Processing chunk " + str(i))
-
-        file = filename
-
-        r = sr.Recognizer()
-
-        # recognize the chunk
-        with sr.AudioFile(file) as source:
-            r.adjust_for_ambient_noise(source)
-            audio_listened = r.listen(source)
-
-        try:
-            rec = r.recognize_google(audio_listened)
-            fh.write(rec + ". ")
-
-        except sr.UnknownValueError:
-            print("Could not understand audio")
-
-        except sr.RequestError as e:
-            print("Could not request results. check your internet connection")
-
-        i += 1
-
-    os.chdir('..')
-
-
-def read_article(file_name):
-    file = open(file_name, "r")
-    filedata = file.readlines()
-    article = filedata[0].split(". ")
-    sentences = []
-
-    for sentence in article:
-        print(sentence)
-        sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
-    sentences.pop()
-
-    return sentences
-
-
-def sentence_similarity(sent1, sent2, stopwords=None):
-    if stopwords is None:
-        stopwords = []
-
-    sent1 = [w.lower() for w in sent1]
-    sent2 = [w.lower() for w in sent2]
-
-    all_words = list(set(sent1 + sent2))
-
-    vector1 = [0] * len(all_words)
-    vector2 = [0] * len(all_words)
-
-    # build the vector for the first sentence
-    for w in sent1:
-        if w in stopwords:
-            continue
-        vector1[all_words.index(w)] += 1
-
-    # build the vector for the second sentence
-    for w in sent2:
-        if w in stopwords:
-            continue
-        vector2[all_words.index(w)] += 1
-
-    return 1 - cosine_distance(vector1, vector2)
-
-
-def build_similarity_matrix(sentences, stop_words):
-    # ye hum banaenge empty similarity matrix
-    similarity_matrix = np.zeros((len(sentences), len(sentences)))
-
-    for idx1 in range(len(sentences)):
-        for idx2 in range(len(sentences)):
-            if idx1 == idx2:  # ignore if both are same sentences
-                continue
-            similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
-
-    return similarity_matrix
-
-
-def generate_summary(file_name, top_n=5):
-    stop_words = stopwords.words('english')
-    summarize_text = []
-
-    sentences = read_article(file_name)
-
-    sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
-
-    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
-    scores = nx.pagerank(sentence_similarity_graph)
-
-    ranked_sentence = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
-
-    for i in range(top_n):
-        summary = summarize_text.append(" ".join(ranked_sentence[i][1]))
-
-    print("Summarize Text: \n", ". ".join(summarize_text))
-
-
 @app.route('/', methods=["GET", "POST"])
 def index():
+    def silence_based_conversion(path):
+        song = AudioSegment.from_wav(path)
+
+        fh = open("recognized.txt", "w+")
+
+        # split track where silence is 0.7 seconds
+        # or more and get chunks
+        chunks = split_on_silence(song,
+                                  # must be silent for at least 0.7 seconds
+                                  # or 500 ms. adjust this value based on user
+                                  # requirement. if the speaker stays silent for
+                                  # longer, increase this value. else, decrease it.
+                                  min_silence_len=700,
+
+                                  # consider it silent if quieter than -42 dBFS
+                                  # adjust this per requirement
+                                  silence_thresh=-42
+                                  )
+
+        try:
+            os.mkdir('audio_chunks')  # Here the data have to be directed to SQL DB
+        except(FileExistsError):
+            pass
+
+        os.chdir('audio_chunks')
+
+        i = 0
+        # process each chunk
+        for chunk in chunks:
+
+            # Create 0.5 seconds silence chunk
+            chunk_silent = AudioSegment.silent(duration=10)
+
+            # add 0.5 sec silence to beginning and
+            # end of audio chunk. This is done so that
+            # it doesn't seem abruptly sliced.
+            audio_chunk = chunk_silent + chunk + chunk_silent
+
+            print("saving chunk{0}.wav".format(i))
+
+            audio_chunk.export("./chunk{0}.wav".format(i), bitrate='192k', format="wav")
+
+            filename = 'chunk' + str(i) + '.wav'
+
+            print("Processing chunk " + str(i))
+
+            file = filename
+
+            r = sr.Recognizer()
+
+            # recognize the chunk
+            with sr.AudioFile(file) as source:
+                r.adjust_for_ambient_noise(source)
+                audio_listened = r.listen(source)
+
+            try:
+                rec = r.recognize_google(audio_listened)
+                fh.write(rec + ". ")
+
+            except sr.UnknownValueError:
+                print("Could not understand audio")
+
+            except sr.RequestError as e:
+                print("Could not request results. check your internet connection")
+
+            i += 1
+
+        os.chdir('..')
+
+    def read_article(file_name):
+        file = open(file_name, "r")
+        filedata = file.readlines()
+        article = filedata[0].split(". ")
+        sentences = []
+
+        for sentence in article:
+            print(sentence)
+            sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+        sentences.pop()
+
+        return sentences
+
+    def sentence_similarity(sent1, sent2, stopwords=None):
+        if stopwords is None:
+            stopwords = []
+
+        sent1 = [w.lower() for w in sent1]
+        sent2 = [w.lower() for w in sent2]
+
+        all_words = list(set(sent1 + sent2))
+
+        vector1 = [0] * len(all_words)
+        vector2 = [0] * len(all_words)
+
+        # build the vector for the first sentence
+        for w in sent1:
+            if w in stopwords:
+                continue
+            vector1[all_words.index(w)] += 1
+
+        # build the vector for the second sentence
+        for w in sent2:
+            if w in stopwords:
+                continue
+            vector2[all_words.index(w)] += 1
+
+        return 1 - cosine_distance(vector1, vector2)
+
+    def build_similarity_matrix(sentences, stop_words):
+        # ye hum banaenge empty similarity matrix
+        similarity_matrix = np.zeros((len(sentences), len(sentences)))
+
+        for idx1 in range(len(sentences)):
+            for idx2 in range(len(sentences)):
+                if idx1 == idx2:  # ignore if both are same sentences
+                    continue
+                similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+
+        return similarity_matrix
+
+    def generate_summary(file_name, top_n=5):
+        stop_words = stopwords.words('english')
+        summarize_text = []
+
+        sentences = read_article(file_name)
+
+        sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+
+        sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+        scores = nx.pagerank(sentence_similarity_graph)
+
+        ranked_sentence = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+
+        for i in range(top_n):
+            summarize_text.append(" ".join(ranked_sentence[i][1]))
+
+        print("Summarize Text: \n", ". ".join(summarize_text))
+        str1 = '. '.join(summarize_text)
+        return str1
+
     transcript = ""
     if request.method == "POST":
         print("FORM DATA RECEIVED")
@@ -177,7 +172,7 @@ def index():
             clip.audio.write_audiofile(r"converted.wav")
             silence_based_conversion(path="converted.wav")
             generate_summary("recognized.txt", 2)
-    return render_template("index.html", transcript=transcript)
+    return render_template("index.html", transcript=generate_summary("recognized.txt", 2) )
 
 
 if __name__ == "__main__":
